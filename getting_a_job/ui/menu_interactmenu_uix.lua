e3a3dd82e3901e5578c5d37c851863eb3b6ea500
archenovalis <archelaus.novalis@gmail.com>
2025-02-25 11:37:58 -0700

    checkpoint

--- getting_a_job/ui/menu_interactmenu_uix.lua
+++ getting_a_job/ui/menu_interactmenu_uix.lua
@@ -1,22 +1,15 @@
 local ModLua = {}
-local interactMenu = {}
-
--- Initialize function to register the callback
-function ModLua.init()
-    interactMenu = Helper.getMenu("InteractMenu")
+function ModLua.init ()
+    local interactMenu = Helper.getMenu("InteractMenu")
     interactMenu.registerCallback("createContentTable_getIsActionValid", ModLua.createContentTable_getIsActionValid)
 end
-
 -- Callback function to validate actions
 function ModLua.createContentTable_getIsActionValid(entry)
     -- Invalidate the "sellships" action
     if entry and entry.type and entry.type == "sellships" then
         return false
     end
-
     -- Return true to allow other actions
     return true
 end
-
--- Initialize ModLua
-ModLua.init()
+return ModLua
